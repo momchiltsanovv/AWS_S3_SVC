dd90108655befc944b571ab7ddeb7eaa4dd455e5
Momchil Tsanov <momchil395@gmail.com>
2025-11-13 22:12:31 +0200

    chore: replace with record

--- src/main/java/com/nexio/aws_s3_svc/dto/S3Response.java
+++ src/main/java/com/nexio/aws_s3_svc/dto/S3Response.java
@@ -2,13 +2,9 @@ package com.nexio.aws_s3_svc.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-import lombok.Data;
 
-@Data
 @Builder
-public class S3Response {
-
-    @JsonProperty("URL")
-    private String URL;
-
+public record S3Response(
+        @JsonProperty("URL") String URL
+){
 }
